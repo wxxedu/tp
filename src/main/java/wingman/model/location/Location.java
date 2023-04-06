de6ef2cc7c1d524f18473378b0eb26dbe9ad6d00
Xiuxuan Wang <36021591+wxxedu@users.noreply.github.com>
2023-04-06 09:53:49 +0800

    fix imports

--- src/main/java/wingman/model/location/Location.java
+++ src/main/java/wingman/model/location/Location.java
@@ -4,18 +4,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import wingman.commons.util.GetUtil;
-import wingman.model.Model;
-import wingman.model.ReadOnlyItemManager;
-import wingman.model.crew.Crew;
 import wingman.model.item.Item;
-import wingman.model.link.Link;
-import wingman.model.pilot.Pilot;
-import wingman.model.plane.Plane;
 
 /**
  * Location is a unit place that the flight can travel to or
- *  arrive at.
+ * arrive at.
  */
 public class Location implements Item {
 
@@ -27,64 +20,34 @@ public class Location implements Item {
                     FlightLocationType.LOCATION_ARRIVAL, 1
             );
 
-    private final Link<CrewLocationType, Crew, ReadOnlyItemManager<Crew>> crewLink;
-    private final Link<PilotLocationType, Pilot, ReadOnlyItemManager<Pilot>> pilotLink;
-    private final Link<PlaneLocationType, Plane, ReadOnlyItemManager<Plane>> planeLink;
     private final String name;
     private final String id;
 
     /**
      * Creates a Location object with the given name.
+     *
      * @param name name of the location
      */
     public Location(
             String name
     ) {
-        this(
-            UUID.randomUUID().toString(),
-            name,
-            new Link<>(
-                    Crew.SHAPE_FOR_LOCATION,
-                    GetUtil.getLazy(Model.class).map(Model::getCrewManager)
-            ),
-            new Link<>(
-                    Pilot.SHAPE_FOR_LOCATION,
-                    GetUtil.getLazy(Model.class).map(Model::getPilotManager)
-            ),
-            new Link<>(
-                    Plane.SHAPE_FOR_LOCATION,
-                    GetUtil.getLazy(Model.class).map(Model::getPlaneManager)
-            )
-        );
+        this(UUID.randomUUID().toString(), name);
     }
 
     /**
      * Creates a Location object from the given id and name
-     * @param id a unique id assigned to the location
+     *
+     * @param id   a unique id assigned to the location
      * @param name the name of the location
-     * @param crewLink the link to the crews that stay in
-     *                 this location
-     * @param pilotLink the link to the pilot that stays
-     *                  in this location
-     * @param planeLink the link to the planes that stays in
-     *                  this location
      */
-    public Location(
-            String id,
-            String name,
-            Link<CrewLocationType, Crew, ReadOnlyItemManager<Crew>> crewLink,
-            Link<PilotLocationType, Pilot, ReadOnlyItemManager<Pilot>> pilotLink,
-            Link<PlaneLocationType, Plane, ReadOnlyItemManager<Plane>> planeLink
-    ) {
+    public Location(String id, String name) {
         this.id = id;
         this.name = name;
-        this.crewLink = crewLink;
-        this.pilotLink = pilotLink;
-        this.planeLink = planeLink;
     }
 
     /**
      * Returns the name of the location in string.
+     *
      * @return the name of the location
      */
     public String getName() {
@@ -103,39 +66,7 @@ public class Location implements Item {
 
     @Override
     public List<String> getDisplayList() {
-        return List.of(
-                String.format("%s", name),
-                String.format("%s: %s\n", "Plane", planeLink.toString()),
-                String.format("%s: %s\n", "Pilots", pilotLink.toString()),
-                String.format("%s: %s\n", "Crew", crewLink.toString())
-        );
-    }
-
-    /**
-     * Returns the crew link.
-     *
-     * @return the link to the crew
-     */
-    public Link<CrewLocationType, Crew, ReadOnlyItemManager<Crew>> getCrewLink() {
-        return crewLink;
-    }
-
-    /**
-     * Returns the pilot link.
-     *
-     * @return the link to the pilot
-     */
-    public Link<PilotLocationType, Pilot, ReadOnlyItemManager<Pilot>> getPilotLink() {
-        return pilotLink;
-    }
-
-    /**
-     * Returns the plane link.
-     *
-     * @return the link to the plane
-     */
-    public Link<PlaneLocationType, Plane, ReadOnlyItemManager<Plane>> getPlaneLink() {
-        return planeLink;
+        return List.of(String.format("%s", name));
     }
 
     /**
